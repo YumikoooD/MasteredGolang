c859fc535f508dd86a267c445deaac19b6e7b5ff
Yumikooo <mikael.sourati@epitech.eu>
2024-08-08 14:26:34 +0200

    refactor: :art: Create subfolder "routes" for clearest code
    the routes subfolder will contains all the function about the routes such has /events page or /home. The routes files will only contains the functions when the is a http method. All this fonctions in the events.go file, also in the route subfolder

---
+++ RestAPI/routes/routes.go
@@ -0,0 +1,9 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	server.GET("/events", getEvents)     // If we have a GET to the /events page, getEvents func is call
+	server.GET("/events/:id", getEvent)  //events/1, events/7...
+	server.POST("/events", createEvents) // If we have a POST to the /events page, createEvents func is call
+}
