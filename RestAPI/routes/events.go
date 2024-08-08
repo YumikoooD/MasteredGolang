e6ace528d234e51eaac5e4c38815961cc833be35
Yumikooo <mikael.sourati@epitech.eu>
2024-08-08 14:45:17 +0200

    feat: :sparkles: You can now update the db when there is the PUT method.

--- RestAPI/routes/events.go
+++ RestAPI/routes/events.go
@@ -49,3 +49,31 @@ func createEvents(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
+
+func updateEvents(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	_, err = models.GetEventByID(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event id"})
+		return
+	}
+
+	var updateEvent models.Event
+	err = context.ShouldBindJSON(&updateEvent)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not parse request data"})
+		return
+	}
+	updateEvent.ID = eventId
+	err = updateEvent.Update()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not update event"})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Event update successfully"})
+}
